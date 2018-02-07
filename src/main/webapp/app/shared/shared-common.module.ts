7abef86541cf99a5e901532b36df1551cf90b58a
yllyx <yunxiang.lon@gmail.com>
2018-02-07 22:30:42 +0800

    can connect to gateway, login and logout

---
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -0,0 +1,42 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import locale from '@angular/common/locales/zh-Hans';
+
+import {
+    WhalehomeSharedLibsModule,
+    JhiLanguageHelper,
+    FindLanguageFromKeyPipe,
+    JhiAlertComponent,
+    JhiAlertErrorComponent
+} from './';
+
+@NgModule({
+    imports: [
+        WhalehomeSharedLibsModule
+    ],
+    declarations: [
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ],
+    providers: [
+        JhiLanguageHelper,
+        Title,
+        {
+            provide: LOCALE_ID,
+            useValue: 'zh-Hans'
+        },
+    ],
+    exports: [
+        WhalehomeSharedLibsModule,
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ]
+})
+export class WhalehomeSharedCommonModule {
+    constructor() {
+        registerLocaleData(locale);
+    }
+}
---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,49 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import {
+    WhalehomeSharedLibsModule,
+    WhalehomeSharedCommonModule,
+    CSRFService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    JhiLoginModalComponent,
+    Principal,
+    HasAnyAuthorityDirective,
+} from './';
+
+@NgModule({
+    imports: [
+        WhalehomeSharedLibsModule,
+        WhalehomeSharedCommonModule
+    ],
+    declarations: [
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        AuthServerProvider,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        WhalehomeSharedCommonModule,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class WhalehomeSharedModule {}
